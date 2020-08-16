4fe9f110ee30f96bd90abb1ea83e9e235000cb01
hoangvu <hoangvu8499@gmail.com>
2020-08-17 01:36:52 +0700

    feature/search_and_get_like_song

--- app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -4,14 +4,13 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.database.sqlite.SQLiteDatabase
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -33,30 +32,31 @@ import kotlinx.android.synthetic.main.layout_home.*
 
 
 class MainActivity : AppCompatActivity(), CreateAlbumDialog.DialogListener {
-    private var mlistAlbum = mutableListOf<album_model>()//Danh sach cac album
-    private var mlistSong = mutableListOf<music_model>()//Danh sach nhac co trong he thong
-    lateinit var relation:relationdbhelper//lop nay chua cac phuong thuc de tuong tac với bảng relation
-    lateinit var album:albumdbhelper//lớp này chứa các phương thức để tương tác với bảng album
-    lateinit var music:musicdbhelper//lop nay chua cac phuong thuc de tuong tac voi bang music
+    private var mListAlbum = mutableListOf<album_model>()//Danh sach cac album
+    private var mListSong = mutableListOf<music_model>()//Danh sach nhac co trong he thong
+    lateinit var relation: relationdbhelper//lop nay chua cac phuong thuc de tuong tac với bảng relation
+    lateinit var album: albumdbhelper//lớp này chứa các phương thức để tương tác với bảng album
+    lateinit var music: musicdbhelper//lop nay chua cac phuong thuc de tuong tac voi bang music
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_home)
-        music= musicdbhelper(this)
-        album=albumdbhelper(this)
+        music = musicdbhelper(this)
+        album = albumdbhelper(this)
         checkUserPermission()
-        mlistAlbum=album.readAllAlbum()//Danh sach cac album co trong database
-        home(topAppBar,this)
-        album(mlistAlbum,rv_album,this)
-
+        mListAlbum = album.readAllAlbum()//Danh sach cac album co trong database
+        home(topAppBar, this)
+        album(mListAlbum, rv_album, this)
         btn_allSong.setOnClickListener {
-            var intent:Intent = Intent(this, LoadAllSong::class.java)
-            startActivity(this,intent,intent.extras)
+            val intent = Intent(this, LoadAllSong::class.java)
+            startActivity(this, intent, intent.extras)
         }
         btn_createAlbum.setOnClickListener {
             showNoticeDialog()
         }
+
     }
+
     fun showNoticeDialog() {
         // Create an instance of the dialog fragment and show it
         val dialog = CreateAlbumDialog()
@@ -66,9 +66,16 @@ class MainActivity : AppCompatActivity(), CreateAlbumDialog.DialogListener {
     private val REQUEST_CODE_ASK_PERMISSION = 123
     private fun checkUserPermission() {
         if (Build.VERSION.SDK_INT >= 23) {
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                !=  PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE_ASK_PERMISSION)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions(
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    REQUEST_CODE_ASK_PERMISSION
+                )
                 return
             }
             loadSong()
@@ -89,21 +96,27 @@ class MainActivity : AppCompatActivity(), CreateAlbumDialog.DialogListener {
             else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
     }
+
     private fun loadSong() {
         val allSongURI = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-        val selection = MediaStore.Audio.Media.IS_MUSIC  + "!= 0"
-        val cursor = contentResolver.query(allSongURI,null , selection, null, null)
-        var list = ArrayList<SongInfo> ()
+        val selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0"
+        val cursor = contentResolver.query(allSongURI, null, selection, null, null)
+        val list = ArrayList<SongInfo>()
         val db = DatabaseHandlerMusic(this)
         if (cursor !== null) {
             if (cursor.moveToFirst()) {
                 do {
-                    val songURL = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))
-                    val songAuthor = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
-                    val songName = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME))
-                    val time = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION))
+                    val songURL =
+                        cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))
+                    val songAuthor =
+                        cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
+                    val songName =
+                        cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME))
+                    val time =
+                        cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION))
                     val imageId = cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)
-                    val song = SongInfo(songName, songAuthor, songURL, time.toInt(), imageId.toLong())
+                    val song =
+                        SongInfo(songName, songAuthor, songURL, time.toInt(), imageId.toLong())
                     db.addSong(song)
                     list.add(song)
                 } while (cursor.moveToNext())
@@ -112,20 +125,20 @@ class MainActivity : AppCompatActivity(), CreateAlbumDialog.DialogListener {
             Service.setSongList(list)
             db.close()
         }
-        mlistSong=music.readAllMusic()//Danh sach bai nhac co trong database
-        song(mlistSong,rv_song,this)
+        mListSong = music.readAllMusic()//Danh sach bai nhac co trong database
+        song(mListSong, rv_song, this)
     }
 
     override fun applyText(text: String) {
         val db = albumdbhelper(this)
         db.insertAlbum(text, false)
-        mlistAlbum=album.readAllAlbum()//Danh sach cac album co trong database
-        album(mlistAlbum,rv_album,this)
+        mListAlbum = album.readAllAlbum()//Danh sach cac album co trong database
+        album(mListAlbum, rv_album, this)
         db.close()
     }
 }
 
-fun home(topAppBar: MaterialToolbar,context: Context) {
+fun home(topAppBar: MaterialToolbar, context: Context) {
     topAppBar.setNavigationOnClickListener {
         // Handle navigation icon press
     }
@@ -136,21 +149,39 @@ fun home(topAppBar: MaterialToolbar,context: Context) {
 //                var intent:Intent = Intent(context, FavoriteActivity::class.java)
 //                startActivity(context,intent,intent.extras)
                 val linearLayout = (context as Activity).findViewById<View>(R.id.linearAction)
-                val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                val inflater =
+                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
                 if ((linearLayout as ViewGroup).childCount === 0) {
                     inflater.inflate(R.layout.area_liked, linearLayout as LinearLayout);
+                    val view: View =
+                        inflater.inflate(R.layout.textview, linearLayout as LinearLayout);
+                    val like = view.findViewById<View>(R.id.btn_like)
+                    like.setOnClickListener {
+                        val intent: Intent = Intent(context, SongLike::class.java)
+                        startActivity(context, intent, intent.extras)
+                    }
                 } else {
                     (linearLayout as ViewGroup).removeViewAt(0)
                     inflater.inflate(R.layout.area_liked, linearLayout as LinearLayout)
                 }
                 true
             }
-            R.id.search -> {
+            R.id.searchItem -> {
                 // Handle search icon press
+                lateinit var music: musicdbhelper//lop nay chua cac phuong thuc de tuong tac voi bang music
                 val linearLayout = (context as Activity).findViewById<View>(R.id.linearAction)
-                val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                val inflater =
+                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
                 if ((linearLayout as ViewGroup).childCount === 0) {
-                    inflater.inflate(R.layout.textview, linearLayout as LinearLayout);
+                    val view: View =
+                        inflater.inflate(R.layout.textview, linearLayout as LinearLayout);
+                    val search = view.findViewById<View>(R.id.search)
+                    search.setOnClickListener {
+                        val nameSearch = view.findViewById<View>(R.id.name_search) as EditText
+                        val intent: Intent = Intent(context, SearchActivity::class.java)
+                        intent.putExtra("nodung", nameSearch.text.toString())
+                        startActivity(context, intent, intent.extras)
+                    }
                 } else {
                     (linearLayout as ViewGroup).removeViewAt(0)
                     inflater.inflate(R.layout.textview, linearLayout as LinearLayout);
@@ -166,19 +197,21 @@ fun home(topAppBar: MaterialToolbar,context: Context) {
     }
 }
 
-fun album(mlistAlbum:MutableList<album_model>,rv_album:RecyclerView,context: Context)
-{
-    var layoutManager: LinearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+fun album(mlistAlbum: MutableList<album_model>, rv_album: RecyclerView, context: Context) {
+    var layoutManager: LinearLayoutManager =
+        LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
     rv_album.layoutManager = layoutManager
     //rv_album.itemAnimator=DefaultItemAnimator
     val adapter = adapteralbum(mlistAlbum)
     rv_album.adapter = adapter
-    rv_album.addItemDecoration(spacealbum(1,2))}
-fun song(mlistSong: MutableList<music_model>, rv_song:RecyclerView, context: Context)
-{
-    var layoutManager: LinearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+    rv_album.addItemDecoration(spacealbum(1, 2))
+}
+
+fun song(mlistSong: MutableList<music_model>, rv_song: RecyclerView, context: Context) {
+    var layoutManager: LinearLayoutManager =
+        LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
     rv_song.layoutManager = layoutManager
     val adapter = adaptersong(mlistSong, context)
     rv_song.adapter = adapter
-    rv_song.addItemDecoration(spacealbum(1,2))
+    rv_song.addItemDecoration(spacealbum(1, 2))
 }
